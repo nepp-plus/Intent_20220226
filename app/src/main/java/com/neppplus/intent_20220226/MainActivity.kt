741b75f894d392f180ed96b0b6df00cd521ac25d
nepp.plus <nepp.plus@gmail.com>
2022-02-26 10:48:38 +0900

    프로젝트 기본 세팅 + 다른화면으로 이동 버튼 추가

--- app/src/main/java/com/neppplus/intent_20220226/MainActivity.kt
+++ app/src/main/java/com/neppplus/intent_20220226/MainActivity.kt
@@ -2,10 +2,20 @@ package com.neppplus.intent_20220226
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnMoveToOther.setOnClickListener {
+
+//            OtherActivity 화면으로 이동
+
+
+
+        }
+
     }
 }
