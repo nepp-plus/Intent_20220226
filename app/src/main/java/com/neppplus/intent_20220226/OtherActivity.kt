a844d2e2c5c7ec86d1aeceda023a1e450302693b
nepp.plus <nepp.plus@gmail.com>
2022-02-26 11:10:17 +0900

    다른 화면에서의 UI 구조 그리기

--- app/src/main/java/com/neppplus/intent_20220226/OtherActivity.kt
+++ app/src/main/java/com/neppplus/intent_20220226/OtherActivity.kt
@@ -2,10 +2,18 @@ package com.neppplus.intent_20220226
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+        btnReturn.setOnClickListener {
+
+//            이전 화면 (메인 화면) 으로 돌아가기
+
+        }
+
     }
 }
