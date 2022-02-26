84a205a7db111ee67cbf43f65cd9409380d67dc5
nepp.plus <nepp.plus@gmail.com>
2022-02-26 14:12:03 +0900

    플레이 스토어 링크 (VIEW)

--- app/src/main/java/com/neppplus/intent_20220226/MainActivity.kt
+++ app/src/main/java/com/neppplus/intent_20220226/MainActivity.kt
@@ -93,6 +93,12 @@ class MainActivity : AppCompatActivity() {
             startActivity(myIntent)
         }
 
+        btnKakaoAppLink.setOnClickListener {
+            val myUri = Uri.parse("market://details?id=com.kakao.talk")
+            val myIntent = Intent( Intent.ACTION_VIEW, myUri )
+            startActivity(myIntent)
+        }
+
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
